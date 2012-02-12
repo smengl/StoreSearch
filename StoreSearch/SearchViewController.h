63754aae123bc150f0c95a08f8f59ed521c9b8fe
Seak Meng Lay <seakmeng90@gmail.com>
2012-02-11 17:59:54 -0800

    Renamed the ViewController to SearchViewController

---
+++ StoreSearch/SearchViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  StoreSearch
+//
+//  Created by Seak Meng Lay on 2/11/12.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface SearchViewController : UIViewController
+
+@end
